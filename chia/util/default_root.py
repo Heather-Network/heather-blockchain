2c59765a025547ad0068bbe684d7e7689411d9e9
Toby Seaman <turbotas@yahoo.com>
2021-10-26 22:42:18 +0100

    SSL update: CA from Chia to Heather

--- chia/util/default_root.py
+++ chia/util/default_root.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("HEATHER_ROOT", "~/.heather/mainnet"))).resolve()
 
-DEFAULT_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_KEYS_ROOT", "~/.heather_keys"))).resolve()
+DEFAULT_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("HEATHER_KEYS_ROOT", "~/.heather_keys"))).resolve()
